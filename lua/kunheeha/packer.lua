50a90aadc4d13a648d4884eddfec5ce1b83b7b12
Kunhee Ha <kunheeha@gmail.com>
2023-03-28 19:47:46 +0100

    use lsp-zero preconfig

--- lua/kunheeha/packer.lua
+++ lua/kunheeha/packer.lua
@@ -31,5 +31,24 @@ return require('packer').startup(function(use)
   use 'shaunsingh/nord.nvim'   -- Nord colourscheme
   use { "catppuccin/nvim", as = "catppuccin" }  -- Catppuccin colourscheme
 
+  -- LSP
+  -- lsp-zero
+  use {
+    'VonHeikemen/lsp-zero.nvim',
+    branch = 'v1.x',
+    requires = {
+      {'neovim/nvim-lspconfig'},
+      {'williamboman/mason.nvim'},
+      {'williamboman/mason-lspconfig.nvim'},
+      {'hrsh7th/nvim-cmp'},
+      {'hrsh7th/cmp-nvim-lsp'},
+      {'hrsh7th/cmp-buffer'},
+      {'hrsh7th/cmp-path'},
+      {'saadparwaiz1/cmp_luasnip'},
+      {'hrsh7th/cmp-nvim-lua'},
+      {'L3MON4D3/LuaSnip'},
+      {'rafamadriz/friendly-snippets'},
+    }
+  }
 
 end)
