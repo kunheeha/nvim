af9ee69787f7302e1964dae37a8d9c422bd2a148
Kunhee Ha <kunheeha@gmail.com>
2023-03-28 14:02:10 +0100

    telescope keymapping

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -1,3 +1,21 @@
+local actions = require('telescope.actions')
+require('telescope').setup{
+  defaults = {
+    mappings = {
+      i = {
+        -- file selection
+        ["<C-j>"] = actions.move_selection_next,
+        ["<C-k>"] = actions.move_selection_previous,
+        ["<C-v>"] = actions.select_vertical,
+        ["<C-s>"] = actions.select_horizontal,
+        ["<C-t>"] = actions.select_tab,
+        -- file preview
+        ["<C-u>"] = actions.preview_scrolling_up,
+        ["<C-d>"] = actions.preview_scrolling_down,
+      }
+    }
+  }
+}
 local builtin = require('telescope.builtin')
 vim.keymap.set('n', '<C-f>', builtin.find_files, {})
 vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
