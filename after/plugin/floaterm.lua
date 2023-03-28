e8398af94fec3eee1db78594f79820ebff0406ec
Kunhee Ha <kunheeha@gmail.com>
2023-03-28 20:59:48 +0100

    floaterm

---
+++ after/plugin/floaterm.lua
@@ -0,0 +1,7 @@
+local opts = { noremap = true, silent = true }
+local keymap = vim.keymap.set
+
+keymap("n", "<leader>f", vim.cmd.FloatermToggle, opts)
+keymap("n", "<leader>fn", vim.cmd.FloatermNext, opts)
+keymap("n", "<leader>fp", vim.cmd.FloatermPrev, opts)
+keymap("n", "<leader>fc", ":FloatermNew --name=", opts)
