41805c27b2d1fdb6fff5cdb45db380afdd799f8f
Kunhee Ha <kunheeha@gmail.com>
2023-05-15 14:09:26 +0100

    gisigns hunk navigation keybinds

--- after/plugin/gitsigns.lua
+++ after/plugin/gitsigns.lua
@@ -50,13 +50,13 @@ require('gitsigns').setup {
     end
 
     -- Navigation
-    map('n', ']c', function()
+    map('n', '<leader>gj', function()
       if vim.wo.diff then return ']c' end
       vim.schedule(function() gs.next_hunk() end)
       return '<Ignore>'
     end, {expr=true})
 
-    map('n', '[c', function()
+    map('n', '<leader>gk', function()
       if vim.wo.diff then return '[c' end
       vim.schedule(function() gs.prev_hunk() end)
       return '<Ignore>'
@@ -70,7 +70,6 @@ require('gitsigns').setup {
     map('n', '<leader>gS', gs.stage_buffer)
     map('n', '<leader>gu', gs.undo_stage_hunk)
     map('n', '<leader>gR', gs.reset_buffer)
-    map('n', '<leader>gp', gs.preview_hunk)
     map('n', '<leader>gb', function() gs.blame_line{full=true} end)
     map('n', '<leader>tb', gs.toggle_current_line_blame)
     map('n', '<leader>gd', gs.diffthis)
