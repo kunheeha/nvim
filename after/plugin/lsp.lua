ad297525ef7ce84b5af8092cc17c87209860dbc6
Kunhee Ha <kunheeha@gmail.com>
2023-05-29 10:00:33 +0100

    enable supertab for completion

--- after/plugin/lsp.lua
+++ after/plugin/lsp.lua
@@ -7,3 +7,13 @@ local lsp = require('lsp-zero').preset({
 
 lsp.nvim_workspace()
 lsp.setup()
+
+local cmp = require('cmp')
+local cmp_action = require('lsp-zero').cmp_action()
+
+cmp.setup({
+  mapping = {
+    ['<Tab>'] = cmp_action.luasnip_supertab(),
+    ['<S-Tab>'] = cmp_action.luasnip_shift_supertab(),
+  }
+})
