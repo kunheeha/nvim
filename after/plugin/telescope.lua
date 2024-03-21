fd4778db86a19a00d79107422488ce279d3edca6
Kunhee Ha <kunheeha@gmail.com>
2024-03-21 10:22:12 +0000

    harpoon config and changes in keymaps to use

--- after/plugin/telescope.lua
+++ after/plugin/telescope.lua
@@ -56,5 +56,5 @@ end
 
 vim.keymap.set('n', '<leader>f', find_files, {})
 vim.keymap.set('n', '<C-g>', builtin.live_grep, {})
-vim.keymap.set('n', '<C-f>', builtin.buffers, {})
+vim.keymap.set('n', '<C-b>', builtin.buffers, {})
 vim.keymap.set('n', '<leader>h', builtin.help_tags, {})
