19b0704abc7a24b64bba841aa1480589f66b23ba
Kunhee Ha <kunheeha@gmail.com>
2024-08-28 00:42:59 +0100

    update telescope version

--- lua/kunheeha/packer.lua
+++ lua/kunheeha/packer.lua
@@ -6,7 +6,7 @@ return require('packer').startup(function(use)
 
   -- Telescope
   use {
-    'nvim-telescope/telescope.nvim', tag = '0.1.1',
+    'nvim-telescope/telescope.nvim', tag = '0.1.8',
     requires = { {'nvim-lua/plenary.nvim'} }
   }
   -- Telescope fzf native
