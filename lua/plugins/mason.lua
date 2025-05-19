038255b18f271231dcfe0c193c440d575a8702c6
Kunhee Ha <kunheeha@gmail.com>
2025-05-19 19:35:35 +0100

    basic mason setup'

---
+++ lua/plugins/mason.lua
@@ -0,0 +1,11 @@
+return {
+  "mason-org/mason.nvim",
+  dependencies = {
+    "mason-org/mason-lspconfig.nvim",
+    "neovim/nvim-lspconfig",
+  },
+  config = function()
+    require("mason").setup()
+    require("mason-lspconfig").setup()
+  end,
+}
