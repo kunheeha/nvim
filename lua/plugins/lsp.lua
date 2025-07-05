edb1858c431551ce8a5ca9e9c956d8206edec9a4
Kunhee Ha <kunheeh@spotify.com>
2025-07-05 23:14:08 +0100

    swap out goto next/prev diagnostic keybinds

--- lua/plugins/lsp.lua
+++ lua/plugins/lsp.lua
@@ -14,8 +14,8 @@ return {
 
         vim.keymap.set("n", "gd", function() vim.lsp.buf.definition() end, opts)
         vim.keymap.set("n", "K", function() vim.lsp.buf.hover() end, opts)
-        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_next() end, opts)
-        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_prev() end, opts)
+        vim.keymap.set("n", "]d", function() vim.diagnostic.goto_next() end, opts)
+        vim.keymap.set("n", "[d", function() vim.diagnostic.goto_prev() end, opts)
         vim.keymap.set("n", "gr", require("telescope.builtin").lsp_references, opts)
         vim.keymap.set("n", "<C-s>", require("telescope.builtin").lsp_document_symbols, opts)
       end,
