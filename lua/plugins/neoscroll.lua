c7cca1b27cb272c079d91596b4fe66aba8588a36
Kunhee Ha <kunheeh@spotify.com>
2025-08-26 23:02:20 +0100

    fix keymap conflict between telescope.buffers and neoscroll

--- lua/plugins/neoscroll.lua
+++ lua/plugins/neoscroll.lua
@@ -2,7 +2,7 @@ return {
   "karb94/neoscroll.nvim",
   config = function()
     require("neoscroll").setup({
-      mappings = { "<C-u>", "<C-d>", "<C-b>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
+      mappings = { "<C-u>", "<C-d>", "<C-y>", "<C-e>", "zt", "zz", "zb" },
       hide_cursor = true,
       stop_eof = true,
       respect_scrolloff = false,
