a0f14b400c7121bc56ec37075bdef99910a9ee7a
Kunhee Ha <kunheeh@spotify.com>
2026-03-01 13:18:55 +0000

    auto set colourscheme based on theme set in config

--- lua/config/init.lua
+++ lua/config/init.lua
@@ -1,6 +1,9 @@
 require("config.keymaps")
 require("config.lazy")
-require("config.colors").set("rose-pine")
+local f = io.open(os.getenv("HOME") .. "/.config/theme/current")
+local mode = f and f:read("*l") or "light"
+if f then f:close() end
+require("config.colors").set(mode == "dark" and "rose-pine" or "zenbones")
 require("config.options")
 require("config.tmux_vim_nav")
 require("config.autocorrect")
