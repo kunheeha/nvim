d145713e267cfeefbbe2d9172676a5f1bd02c5ec
Kunhee Ha <kunheeh@spotify.com>
2026-03-07 19:04:45 +0000

    obsidian plugin

---
+++ lua/plugins/obsidian.lua
@@ -0,0 +1,34 @@
+return {
+  "obsidian-nvim/obsidian.nvim",
+  version = "*",
+  ft = "markdown",
+  dependencies = {
+    "nvim-lua/plenary.nvim",
+    "nvim-telescope/telescope.nvim",
+  },
+  config = function()
+    require("obsidian").setup({
+      workspaces = {
+        {
+          name = "notes",
+          path = "~/Notes",
+        },
+      },
+      ui = { enable = false },
+    })
+
+    local opts = { noremap = true, silent = true }
+    vim.keymap.set("n", "<leader>oo", "<cmd>Obsidian open<CR>", opts)
+    -- vim.keymap.set("n", "<leader>ot", "<cmd>Obsidian today<CR>", opts)
+    -- vim.keymap.set("n", "<leader>oy", "<cmd>Obsidian yesterday<CR>", opts)
+    -- vim.keymap.set("n", "<leader>od", "<cmd>Obsidian dailies<CR>", opts)
+    vim.keymap.set("n", "<leader>oc", "<cmd>Obsidian toggle_checkbox<CR>", opts)
+    vim.keymap.set("n", "<CR>", function()
+      if require("obsidian").util.cursor_on_markdown_link() then
+        vim.cmd("Obsidian follow_link")
+      else
+        vim.api.nvim_feedkeys(vim.api.nvim_replace_termcodes("<CR>", true, false, true), "n", false)
+      end
+    end, opts)
+  end,
+}
