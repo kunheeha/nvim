93ed82c16956f688d8929aa47de540e4c79626e6
Kunhee Ha <kunheeh@spotify.com>
2026-08-03 16:22:58 +0100

    treesitter markdown fix

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -3,20 +3,42 @@ return {
   branch = "master",
   build = ":TSUpdate",
   config = function()
-    -- keep plugin for parser/query files + :TSInstall/:TSUpdate
-    -- highlighting via built-in vim.treesitter (not plugin's highlight module)
     require("nvim-treesitter.configs").setup({
-      ensure_installed = { "lua", "vim", "vimdoc", "java", "latex", "bibtex", "go", "gomod", "gosum", "gowork" },
+      ensure_installed = { "lua", "vim", "vimdoc", "java", "latex", "bibtex", "go", "gomod", "gosum", "gowork", "markdown", "markdown_inline" },
       sync_install = true,
       auto_install = true,
-      highlight = { enable = false },
+      highlight = { enable = true },
     })
 
-    vim.api.nvim_create_autocmd("FileType", {
-      group = vim.api.nvim_create_augroup("builtin_treesitter_hl", { clear = true }),
-      callback = function(args)
-        pcall(vim.treesitter.start, args.buf)
-      end,
-    })
+    -- nvim-treesitter's markdown injection query uses #set-lang-from-info-string!
+    -- which crashes during range-based parsing on certain files (nil node in
+    -- get_range). Override with the bundled query that uses @injection.language.
+    vim.treesitter.query.set("markdown", "injections", [[
+      (fenced_code_block
+        (info_string
+          (language) @injection.language)
+        (code_fence_content) @injection.content)
+
+      ((html_block) @injection.content
+        (#set! injection.language "html")
+        (#set! injection.combined)
+        (#set! injection.include-children))
+
+      ((minus_metadata) @injection.content
+        (#set! injection.language "yaml")
+        (#offset! @injection.content 1 0 -1 0)
+        (#set! injection.include-children))
+
+      ((plus_metadata) @injection.content
+        (#set! injection.language "toml")
+        (#offset! @injection.content 1 0 -1 0)
+        (#set! injection.include-children))
+
+      ([
+        (inline)
+        (pipe_table_cell)
+      ] @injection.content
+        (#set! injection.language "markdown_inline"))
+    ]])
   end,
 }
